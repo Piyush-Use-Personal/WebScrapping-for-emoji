6a5c30200ccbffa194494961f09902c6e1dc6983
Piyush <piyush115dubey@gmail.com>
2021-01-02 19:22:19 +0530

    Added more items in not present emojis

--- notPresentEmojis.js
+++ notPresentEmojis.js
@@ -18,5 +18,36 @@ module.exports = [
         "replaceItem" : '<i class="em em-slightly_smiling_face" aria-role="presentation" aria-label="SLIGHTLY SMILING FACE"></i>',
         "engName" : "SMILEY FACE",
         "emoji" : "☺"
+    },
+    // testing two 
+    {
+        "replaceItem" : '<i class="em em-couple_with_heart" aria-role="presentation" aria-label="COUPLE WITH HEART"></i>',
+        "engName" : "COUPLE WITH HEART",
+        "emoji" : "💑"
+    },
+    {
+        "replaceItem" : '<i class="em em-family" aria-role="presentation" aria-label="FAMILY"></i>',
+        "engName" : "FAMILY",
+        "emoji" : "👪"
+    },
+    {
+        "replaceItem" : '<i class="em em-woman-walking" aria-role="presentation" aria-label=""></i>',
+        "engName" : "walking",
+        "emoji" : "🙎"
+    },
+    {
+        "replaceItem" : '<i class="em em-pray" aria-role="presentation" aria-label="PERSON WITH FOLDED HANDS"></i>',
+        "engName" : "PREY",
+        "emoji" : "🙇"
+    },
+    {
+        "replaceItem" : '<i class="em em-phone" aria-role="presentation" aria-label="BLACK TELEPHONE"></i>',
+        "engName" : "BLACK TELEPHONE",
+        "emoji" : "☎"
+    },
+    {
+        "replaceItem" : '<i class="em em-black_heart" aria-role="presentation" aria-label="BLACK HEART"></i>',
+        "engName" : "BLACK HEART",
+        "emoji" : "❤"
     }
 ]
